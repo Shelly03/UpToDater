019d9fb11ba13aff2e5ba8a4abb3d8afc989524a
Shelly03 <shelly3278@gmail.com>
2023-03-30 00:11:36 +0300

    added unfinished server gui

---
+++ Code/server_gui.py
@@ -0,0 +1,14 @@
+import customtkinter
+import tkinter
+import server
+
+class server_gui:
+    
+    def __init__(self):
+        customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+        customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+        self.app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+        self.app.geometry("1200x800")
+        
+        self.app.mainloop()
