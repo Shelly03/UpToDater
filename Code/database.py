bbd8e14b20b44431caf67513774965b339b2df8b
Shelly03 <1@somerandom.mail>
2023-06-15 02:41:04 +0300

    added prints to debug

--- Code/database.py
+++ Code/database.py
@@ -1,16 +1,15 @@
-
 import sqlite3
 from cryptography.fernet import Fernet
 
-class database:
 
+class database:
     def __init__(self):
         # key
-        file = open(r'sources\Files\key.key', 'rb') # rb = read bytes
-        self.key  = file.read()
+        file = open(r"sources\Files\key.key", "rb")  # rb = read bytes
+        self.key = file.read()
         file.close()
         self.fernet = Fernet(self.key)
-        
+
         # create connection and cursor for the db
         self.db_name = "ipconnections.db"
         db_conn = sqlite3.connect(self.db_name)
@@ -19,7 +18,7 @@ class database:
         # tables names
         self.conn_table_name = "Connections"
         self.info_table_name = "CompInfo"
-        
+
         # columns for the table that describes the different computers on the net
         self.conn_table_columns = [
             "id",
@@ -27,15 +26,9 @@ class database:
             "mac_address",
             "connection_status",
         ]
-        
+
         # columns for the table that saves the computer's updates on their performance
-        self.info_table_columns = [
-            "id", 
-            "cpu", 
-            "temperature", 
-            "memory", 
-            "check_time"
-        ]
+        self.info_table_columns = ["id", "cpu", "temperature", "memory", "check_time"]
 
         # create connections table
         db_cursor.execute(
@@ -45,8 +38,10 @@ class database:
                     mac_address TEXT NOT NULL,
                     connection_status TEXT NOT NULL)"""
         )
-        
-        db_cursor.execute(f"UPDATE {self.conn_table_name} SET connection_status = 'off'")
+
+        db_cursor.execute(
+            f"UPDATE {self.conn_table_name} SET connection_status = 'off'"
+        )
 
         # create computers info table
         db_cursor.execute(
@@ -64,29 +59,30 @@ class database:
         db_conn.close()
 
     def get_id(self, ip, rows):
+        print('ip: ', ip)
         for row in rows:
             decrypted_ip = self.fernet.decrypt(row[1]).decode()
+            print("dec ip: ",decrypted_ip)
             if decrypted_ip == ip:
                 return row[0]
-
+        return 'not found'
 
     def update_connection(self, ip, mac, status):
         # create new connection and cursor
         db_conn = sqlite3.connect(self.db_name)
         db_cursor = db_conn.cursor()
-        
-        
+
         encrypted_ip = self.fernet.encrypt(ip.encode())
         encrypted_mac = self.fernet.encrypt(mac.encode())
-                
+
         # check if the ip is already in the table
         db_cursor.execute(
-                        f"SELECT {self.conn_table_columns[0]},{self.conn_table_columns[1]} FROM {self.conn_table_name}",
-                    )
+            f"SELECT {self.conn_table_columns[0]},{self.conn_table_columns[1]} FROM {self.conn_table_name}",
+        )
         rows = db_cursor.fetchall()
-        
+
         wanted_id = self.get_id(ip, rows)
-        
+
         # check if new ip or not
         if wanted_id is not None:
             # if exist update connection status to the one given
@@ -102,26 +98,26 @@ class database:
             )
         db_conn.commit()
         db_conn.close()
-        
+
     def add_data(self, data):
         # create new connection and cursor
         db_conn = sqlite3.connect(self.db_name)
         db_cursor = db_conn.cursor()
-        
+        print('connected')
         # find the id of the ip
         db_cursor.execute(
             f"SELECT {self.conn_table_columns[0]}, {self.conn_table_columns[1]} FROM {self.conn_table_name}",
         )
         rows = db_cursor.fetchall()
-
+        print('got rows ', rows)
         wanted_id = self.get_id(data[0], rows)
-        
-        # add the colomn with all of the info
+        print('wanted id: ', wanted_id)
+        # add the row with all of the info
         values = [wanted_id] + data[1:]
         db_cursor.execute(
             f"INSERT INTO {self.info_table_name} ({', '.join(self.info_table_columns)}) VALUES (?, ?, ?, ?, ?)",
             (values),
         )
+        print('inserted data')
         db_conn.commit()
         db_conn.close()
-
--- Code/server.py
+++ Code/server.py
@@ -11,15 +11,14 @@ from database import database
 
 MAIN_PORT = 65432
 ALERT_PORT = 65431
-ADMIN_SETTINGS_PATH = 'sources\Files\Admin.txt'
+ADMIN_SETTINGS_PATH = "sources\Files\Admin.txt"
 
 # dict to save the ip and socket of computers
 COMPUTERS = {}
 
-class server:
-    
-    def __init__(self):
 
+class server:
+    def __init__(self):
         # start main socket
         self.main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.main_socket.bind(("0.0.0.0", MAIN_PORT))
@@ -37,28 +36,26 @@ class server:
 
         # create DB handler instance
         self.database = database()
-        
 
-
-    def get_comp_info(self, client_conn, client_address): 
-        self.update_database_connection(client_address, 'on')
+    def get_comp_info(self, client_conn, client_address):
         try:
             while True:
-
                 try:
                     data = client_conn.recv(4096).decode()
-                    
-                    if data == 'bye':
+
+                    if data == "bye":
                         break
-                    
+
                     data = data.split(",")
-                    
+
                     # append the data to the global list so it will be added to the database
-                    if len(data) == 5:
+                    if len(data) == 4:
+                        print('before lock 54')
                         self.lock.acquire()
-                        self.database.add_data(data)
+                        self.database.add_data([client_address[0], *data])
                         self.lock.release()
-                        
+                        print('after lock 58')
+
                     # forbidden process running
                     if len(data) == 2:
                         forbidden_process = data[1]
@@ -82,13 +79,14 @@ class server:
         finally:
             # finally close the socket
             client_conn.close()
-            self.update_database_connection(client_address, 'off')
+            self.update_database_connection(client_address, "off")
+            print('before lock 82')
             self.lock.acquire()
             global COMPUTERS
             COMPUTERS.pop(client_address[0])
             self.lock.release()
+            print('after lock 87')
             print("DONE WITH THIS THREAD")
-
 
     def get_mac_address(self, ip_address):
         try:
@@ -106,9 +104,7 @@ class server:
             )
         return mac_address
 
-
     def update_database_connection(self, client_address, status):
-        self.lock.acquire()
         self.database.update_connection(
             *[
                 client_address[0],
@@ -116,8 +112,6 @@ class server:
                 status,
             ]
         )
-        self.lock.release()
-
 
     def run(self):
         while True:
@@ -125,14 +119,17 @@ class server:
             conn, addr = self.main_socket.accept()
             # accept a new client - alert socket
             alert_conn, alert_addr = self.alert_socket.accept()
-            
-            print('> NEW CONNECTION ', addr)
-            
+
+            print("> NEW CONNECTION ", addr)
+
             # put the socket in a global dict to ask for information
+            print('before lock 125')
             self.lock.acquire()
+            self.update_database_connection(addr, "on")
             global COMPUTERS
             COMPUTERS[addr[0]] = conn
             self.lock.release()
+            print('after lock 127')
 
             conn.send(json.dumps(self.get_admin_settings(ADMIN_SETTINGS_PATH)).encode())
 
@@ -149,66 +146,80 @@ class server:
             info_thread.start()
 
     def get_info_from_computer(self, ip, info):
+        print('here')
         global COMPUTERS
         try:
+            print('before lock 148')
             self.lock.acquire()
             socket = COMPUTERS[ip]
             self.lock.release()
+            print('after lock 150')
 
             # Send the information request to the client
+            print(ip, " sent ", info)
             socket.send(info.encode())
+            print("sent", info)
 
-            # Receive the total number of packets
+            '''# Receive the total number of packets
             got_number_of_packets = False
             total_packets = 0
             while not got_number_of_packets:
                 try:
-                    total_packets = int(socket.recv(4).decode())
+                    total_packets = int(socket.recv(1024))
+                    print(total_packets)
                     got_number_of_packets = True
                 except:
                     pass
             # Receive and combine the data packets
             received_packets = []
             for packet_number in range(total_packets):
-                data_packet = socket.recv(4096)
-                print('got packet')
+                data_packet = socket.recv(1024)
+                print("got packet")
                 received_packets.append(data_packet)
+            print("done")
 
             # Combine the received packets
             combined_data = b"".join(received_packets)
-            
-            # Return the combined data to the caller
-            return combined_data.decode()
 
+            # Return the combined data to the caller
+            print(combined_data.decode())
+            return combined_data.decode()'''
+            data = socket.recv(65536).decode()
+            print(data)
+            print(type(data))
+            return data
+        
         except Exception as e:
             print(e)
-            
-            
+
     def get_admin_settings(self, path):
         admin_data = {
-        "max_cpu": None,
-        "max_cpu_temp": None,
-        "max_mem": None,
-        "forbidden_processes": [],
-        "email": None
+            "max_cpu": None,
+            "max_cpu_temp": None,
+            "max_mem": None,
+            "forbidden_processes": [],
+            "email": None,
         }
-    
-        with open(path, 'r') as file:
+
+        with open(path, "r") as file:
             lines = file.readlines()
-            
+
             for line in lines:
                 line = line.strip()
-                
+
                 if line.startswith("max_cpu:"):
                     admin_data["max_cpu"] = int(line.split(":")[1])
                 elif line.startswith("max_cpu_temp:"):
                     admin_data["max_cpu_temp"] = int(line.split(":")[1])
                 elif line.startswith("max_mem:"):
                     admin_data["max_mem"] = int(line.split(":")[1])
-                elif line.startswith('"') and line.endswith('"') and line != '"process 1"':
+                elif (
+                    line.startswith('"')
+                    and line.endswith('"')
+                    and line != '"process 1"'
+                ):
                     admin_data["forbidden_processes"].append(line.strip('"'))
                 elif line.startswith("email:"):
                     admin_data["email"] = line.split(":")[1].strip('"')
-    
-        return admin_data
 
+        return admin_data
